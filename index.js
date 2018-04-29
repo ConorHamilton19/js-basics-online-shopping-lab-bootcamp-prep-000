8dfb9bc69a5a9dd98bd21817a6af97e58fc3d4e2
Conor Hamilton <conhamil19@gmail.com>
2018-04-29 01:28:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({[item]: Math.floor(Math.random()*100+1)})
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length===0){
+    console.log('Your shopping cart is empty.')
+  }else if (cart.length===1){
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`)
+  }else if (cart.length===2){
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)
+  }else{
+    for(var i=0; i<cart.length; i++){
+      
+    }
+  }
 }
 
 function total() {
